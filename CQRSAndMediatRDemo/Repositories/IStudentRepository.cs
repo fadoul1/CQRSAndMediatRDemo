aed4d7664ca1e3eb31c9b57bdcd19a0e79d1edeb
Fad OURO AGORO <fadouro@gmail.com>
2023-01-11 18:15:35 +0000

    Add project files.

---
+++ CQRSAndMediatRDemo/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using CQRSAndMediatRDemo.Models;
+
+namespace CQRSAndMediatRDemo.Repositories;
+
+public interface IStudentRepository
+{
+    public Task<List<StudentDetails>> GetStudentListAsync();
+    public Task<StudentDetails> GetStudentByIdAsync(int Id);
+    public Task<StudentDetails> AddStudentAsync(StudentDetails studentDetails);
+    public Task<int> UpdateStudentAsync(StudentDetails studentDetails);
+    public Task<int> DeleteStudentAsync(int Id);
+}
